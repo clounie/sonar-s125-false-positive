8e180385c78fb7d6c2c3bab87eff3a252072c07b
clounie <bruce.w.clounie@gmail.com>
2022-09-29 10:09:12 -0600

    initial commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,8 @@
+package org.example;
+
+public class Main {
+  // This would be something like { "MyField": 5 }
+  public static void main(String[] args) {
+    System.out.println("Hello world!");
+  }
+}
