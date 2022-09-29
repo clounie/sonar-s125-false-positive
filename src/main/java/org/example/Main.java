ef4fe51ba8338b95acc29126f0f519cda40a1cbb
Bruce Clounie <bruce.w.clounie@gmail.com>
2022-09-29 10:11:27 -0600

    Update Main.java

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,6 +1,7 @@
 package org.example;
 
 public class Main {
+  // S125 issue is in the next line. Line has JSON, it pretends JSON is code. Not true.
   // This would be something like { "MyField": 5 }
   public static void main(String[] args) {
     System.out.println("Hello world!");
